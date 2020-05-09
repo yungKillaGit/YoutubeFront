06badebcd4209329b891f070924ffb9728a704ab
yungKillaGit <59793907+yungKillaGit@users.noreply.github.com>
2020-05-09 20:13:44 +0300

    update app component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Grid } from '@material-ui/core';
 import Header from '../header/header';
+import Sidebar from '../sidebar/sidebar';
+import './app.css';
 
 const App = () => (
+  <>
     <Header/>
+    <Grid container wrap="nowrap" justify="space-between" className="wrap-hole">
+      <Sidebar/>
+      <section className="container__main">
+        <header className="ml-3">
+          <h1>Видео</h1>
+        </header>
+        <Grid container item justify="space-around">
+          {[...Array(16)].map(() => (
+            <div className="square"/>
+          ))}
+        </Grid>
+      </section>
+    </Grid>
+  </>
 );
 
 export default App;
