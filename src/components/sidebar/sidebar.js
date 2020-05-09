25857ca5f0ff2ffcbdc8dc3c422f51a2b9ec6aeb
yungKillaGit <59793907+yungKillaGit@users.noreply.github.com>
2020-05-09 20:14:57 +0300

    add sidebar component

---
+++ src/components/sidebar/sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Grid } from '@material-ui/core';
+import LinkList from '../link-list/link-list';
+import './sidebar.css';
+import { getMainLinks, getFooterLinks } from '../../utils';
+
+const mainLinks = getMainLinks();
+const footerLinks = getFooterLinks();
+
+const Sidebar = () => (
+  <Grid container wrap="nowrap" direction="column" className="vertical-nav-bar w-100">
+    <LinkList items={mainLinks}/>
+    <Grid component="footer" container direction="column">
+      <LinkList items={footerLinks}/>
+    </Grid>
+  </Grid>
+);
+
+export default Sidebar;
