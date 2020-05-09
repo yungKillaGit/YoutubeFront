43fecf673419bcd7b691136a6b9e3365da3ce13b
yungKillaGit <59793907+yungKillaGit@users.noreply.github.com>
2020-05-09 20:14:13 +0300

    update header component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,50 +3,40 @@ import {
   Grid,
   CardMedia,
   TextField,
-  makeStyles,
   Avatar,
 } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 import SearchIcon from '@material-ui/icons/Search';
 import logo from '../images/logo.png';
+import './header.css';
 
-const useStyles = makeStyles(() => ({
-  root: {
-    flex: '1 0 auto',
-  },
-}));
-
-const Header = () => {
-  const classes = useStyles();
-
-  return (
-    <header className="mt-3 ml-3 mr-3">
-      <Grid container justify="space-between" wrap="nowrap" className={classes.root}>
-        <Grid container item xs={3}>
-          <Grid item direction="column" xs={3}>
-            <MenuIcon/>
-          </Grid>
-          <Grid item direction="column" xs={9}>
-            <CardMedia image={logo} className="w-25 h-50"/>
-          </Grid>
+const Header = () => (
+  <header className="mt-3 ml-3 mr-3">
+    <Grid container justify="space-between" wrap="nowrap" className="main-header-content">
+      <Grid container item xs={3}>
+        <Grid container item direction="column" xs={3}>
+          <MenuIcon/>
         </Grid>
-        <Grid container item xs={6}>
-          <Grid item xs={10}>
-            <TextField placeholder="Введите запрос" fullWidth/>
-          </Grid>
-          <Grid item xs={2}>
-            <IconButton type="submit" aria-label="search">
-              <SearchIcon />
-            </IconButton>
-          </Grid>
-        </Grid>
-        <Grid container item xs={3} justify="flex-end">
-          <Avatar>A</Avatar>
+        <Grid container item direction="column" xs={9}>
+          <CardMedia image={logo} className="w-25 h-50"/>
         </Grid>
       </Grid>
-    </header>
-  );
-};
+      <Grid container item xs={6}>
+        <Grid item xs={10}>
+          <TextField placeholder="Введите запрос" fullWidth/>
+        </Grid>
+        <Grid item xs={2}>
+          <IconButton type="submit" aria-label="search">
+            <SearchIcon />
+          </IconButton>
+        </Grid>
+      </Grid>
+      <Grid container item xs={3} justify="flex-end">
+        <Avatar>A</Avatar>
+      </Grid>
+    </Grid>
+  </header>
+);
 
 export default Header;
