99a44e498eee078992234ee55845bf4acf07f849
yungKillaGit <59793907+yungKillaGit@users.noreply.github.com>
2020-05-10 17:18:52 +0300

    add authentication

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import { Grid } from '@material-ui/core';
-import Header from '../header/header';
-import Sidebar from '../sidebar/sidebar';
-import './app.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import Home from '../home/home';
+import LoginPage from '../login-page/login-page';
+import RegisterPage from '../register-page/register-page';
+import { AuthContext } from '../../context/auth';
 
-const App = () => (
-  <>
-    <Header/>
-    <Grid container wrap="nowrap" justify="space-between" className="wrap-hole">
-      <Sidebar/>
-      <section className="container__main">
-        <header className="ml-3">
-          <h1>Видео</h1>
-        </header>
-        <Grid container item justify="space-around">
-          {[...Array(16)].map(() => (
-            <div className="square"/>
-          ))}
-        </Grid>
-      </section>
-    </Grid>
-  </>
-);
+const App = () => {
+  const existingToken = localStorage.getItem('authToken');
+  const existingUserId = localStorage.getItem('userId');
+  const existingUserName = localStorage.getItem('userName');
+
+  const isAuthenticated = existingToken !== null && existingToken !== 'undefined';
+  const [token, setToken] = useState(existingToken);
+  const [user, setUser] = useState({ id: existingUserId, name: existingUserName });
+
+  axios.interceptors.request.use((options) => {
+    if (isAuthenticated) {
+      options.headers.Authorization = `Bearer ${existingToken}`;
+    }
+    return options;
+  });
+  const setAuthInfo = (authToken) => {
+    const decodedJwt = jwt_decode(authToken);
+
+    localStorage.setItem('authToken', authToken);
+    localStorage.setItem('userId', decodedJwt.id);
+    localStorage.setItem('userName', decodedJwt.userName);
+
+    setToken(authToken);
+    setUser({ id: decodedJwt.id, name: decodedJwt.userName });
+  };
+
+  return (
+    <AuthContext.Provider value={{
+      token, setAuthInfo, isAuthenticated, user,
+    }}
+    >
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={LoginPage} />
+        <Route exact path="/register" component={RegisterPage} />
+      </Router>
+    </AuthContext.Provider>
+  );
+};
 
 export default App;
--- src/components/login-page/login-page.js
+++ src/components/login-page/login-page.js
@@ -1,22 +1,80 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import {
   Container, TextField, Grid, Button,
 } from '@material-ui/core';
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+import { useAuth } from '../../context/auth';
+import { formatErrors } from '../../utils';
 
-const LoginPage = () => (
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState();
+  const [isLogged, setIsLogged] = useState(false);
+  const { setAuthInfo } = useAuth();
+
+  const login = () => {
+    axios.post('https://localhost:5001/api/auth/login', {
+      email,
+      password,
+    }).then((response) => {
+      if (response.status === 200) {
+        const { authToken } = response.data.token;
+        setAuthInfo(authToken);
+        setIsLogged(true);
+      }
+    }).catch((error) => {
+      setErrors(formatErrors(error.response.data));
+    });
+  };
+
+  if (isLogged) {
+    return <Redirect to="/"/>;
+  }
+
+  return (
     <Container component="form" maxWidth="sm" className="mt-5 border">
       <Grid container direction="column">
         <h3>Вход</h3>
         <h5>Перейдите на Artur`s YouTube</h5>
-        <TextField label="Адрес эл.почты"/>
-        <TextField label="Пароль"/>
+        {
+          errors ? (
+            <>
+              <TextField
+                error
+                label="Адрес эл.почты"
+                helperText={errors.Email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                error
+                label="Пароль"
+                helperText={errors.Password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </>
+          ) : (
+            <>
+            <TextField
+              label="Адрес эл.почты"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              label="Пароль"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            </>
+          )
+        }
         <Grid container justify="space-between" className="mt-4 mb-2">
-          <Link to="register">Создать аккаунт</Link>
-          <Button color="primary" variant="contained">Войти</Button>
+          <Link to="/register">Создать аккаунт</Link>
+          <Button color="primary" variant="contained" onClick={login}>Войти</Button>
         </Grid>
       </Grid>
     </Container>
-);
+  );
+};
 
 export default LoginPage;
