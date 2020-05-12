584d571774e938e8e7e63e124ec1c778f30532c1
yungKillaGit <59793907+yungKillaGit@users.noreply.github.com>
2020-05-12 10:07:04 +0300

    add image capture component

---
+++ src/components/image-capture/image-capture.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { IconButton, makeStyles } from '@material-ui/core';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+
+const useStyles = makeStyles(() => ({
+  input: {
+    display: 'none',
+  },
+}));
+
+const ImageCapture = ({ setImage }) => {
+  const classes = useStyles();
+  const handleCapture = ({ target }) => {
+    setImage(target.files[0]);
+  };
+  return (
+    <>
+      <input
+        accept="image/*"
+        id="icon-button-photo"
+        onChange={handleCapture}
+        className={classes.input}
+        type="file"
+      />
+      <label htmlFor="icon-button-photo">
+        <IconButton color="primary" component="span">
+          <PhotoCamera />
+        </IconButton>
+      </label>
+    </>
+  );
+};
+
+export default ImageCapture;
